40c059c14f96031a75333132609332ee2cf2a7f9
zubinchopra <zubin.msvv@gmail.com>
2017-04-20 17:24:14 -0700

    Evaluate In Progress

--- p2.py
+++ p2.py
@@ -1,5 +1,6 @@
 lhs = ["+", "3", "+", "7", "-", "2", "*", "x"]
-operators =[]
+operators = []
+value = []
 x = float(lhs[1])
 sum = 0
 array_length = len(lhs)
@@ -11,22 +12,47 @@ def operatorFunc():
                 if(lhs[num] == "+" and (num + 2 <= array_length - 1) and (lhs[num + 2] == "*" or lhs[num + 2] == "/")):
                     if(num + 2 <= array_length - 1):
                         if(lhs[num + 2] == "*"):
-                            print("Divide " + lhs[num + 1] + " from both sides")
+                            operators.append("Divide " + lhs[num + 1] + " from both sides")
+                            value.append(float(lhs[num + 1]))
                         elif(lhs[num + 2] == "/"):
-                            print("Multiply " + lhs[num + 1] + " from both sides")
+                            operators.append("Multiply " + lhs[num + 1] + " from both sides")
+                            value.append(float(lhs[num + 1]))
                 elif(lhs[num] == "-" and (num + 2 <= array_length - 1) and (lhs[num + 2] == "*" or lhs[num + 2] == "/")):
                     if(num + 2 <= array_length - 1):
                         if(lhs[num + 2] == "*"):
-                            print("Divide " + lhs[num + 1] + " from both sides")
+                            operators.append("Divide " + lhs[num + 1] + " from both sides")
+                            value.append(float(lhs[num + 1]))
                         elif(lhs[num + 2] == "/"):
-                            print("Multiply " + lhs[num + 1] + " from both sides")
+                            operators.append("Multiply " + lhs[num + 1] + " from both sides")
+                            value.append(float(lhs[num + 1]))
                 elif(lhs[num] == "-"):
-                    print("Add " + lhs[num + 1] + " from both sides")
+                    operators.append("Add " + lhs[num + 1] + " from both sides")
+                    value.append(float(lhs[num + 1]))
                 elif(lhs[num] == "+"):
-                    print("Subtract " + lhs[num + 1] + " from both sides")
+                    operators.append("Subtract " + lhs[num + 1] + " from both sides")
+                    value.append(float(lhs[num + 1]))
                 elif(lhs[num] == "*"):
-                    print("Divide " + lhs[num + 1] + " from both sides")
+                    operators.append("Divide " + lhs[num + 1] + " from both sides")
+                    value.append(float(lhs[num + 1]))
                 else:
-                    print("Multiply " + lhs[num + 1] + " from both sides")
+                    operators.append("Multiply " + lhs[num + 1] + " from both sides")
+                    value.append(float(lhs[num + 1]))
+
+def evaluate(index):
+    option = operators[index]][:1]
+    if(option == "S"):
+        option = "+"
+    elif(option == "A"):
+        option = "-"
+    elif(option == "M"):
+        option = "/"
+    else:
+        option = "*"
+    for num in range (0, array_length - 1):
+        if(option == "D" or option == "M"):
+            if(lhs[num] == value[index] and lhs[num + 1] == option)
+
 
 operatorFunc()
+print(operators)
+print(value)
