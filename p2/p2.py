bf5eb1670df7f19d080668b6887bcb73ae0d4d44
Anoop Narra <narraanoop@gmail.com>
2017-04-22 20:04:12 -0700

    The viz mostly works, but for some reason it breaks the code ifyou run the operator 0 before 1 or 2. I think thats an error with the p2.py file. I have been constantly texting you but you do not respond, so here's what I did for this many hours. You have not fixed your end on the division and stuff. If you want to see it working, run 2,1,0. It works that way.

--- p2/p2.py
+++ p2/p2.py
@@ -1,46 +1,70 @@
+SOLUZION_VERSION = "1.0"
+PROBLEM_NAME = "Linear Equation Solver"
+PROBLEM_VERSION = "1.1"
+PROBLEM_AUTHORS = ['Anoop Narra, Zubin']
+PROBLEM_CREATION_DATE = "20-APR-2017"
+
+PROBLEM_DESC=\
+ '''The <b>"Missionaries and Cannibals"</b> problem is a traditional puzzle
+in which the player starts off with three missionaries and three cannibals
+on the left bank of a river.  The object is to execute a sequence of legal
+moves that transfers them all to the right bank of the river.  In this
+version, there is a boat that can carry at most three people, and one of
+them must be a missionary to steer the boat.  It is forbidden to ever
+have one or two missionaries outnumbered by cannibals, either on the
+left bank, right bank, or in the boat.  In the formulation presented
+here, the computer will not let you make a move to such a forbidden situation, and it
+will only show you moves that could be executed "safely."
+'''
+
+
 lhs = ["+", "3", "+", "7", "-", "2", "*", "x"]
-operators = []
+OPERATORS = []
 value = []
 x = float(lhs[1])
 sum = 0
+INITIAL_STATE = lhs
 
 def operatorFunc():
+    global OPERATORS
     array_length = len(lhs)
+    OPERATORS = []
     for num in range (0, array_length - 1):
         if(num % 2 == 0):
             if(lhs[num + 1] != "x"):
                 if(lhs[num] == "+" and (num + 2 <= array_length - 1) and (lhs[num + 2] == "*" or lhs[num + 2] == "/")):
                     if(num + 2 <= array_length - 1):
                         if(lhs[num + 2] == "*"):
-                            operators.append("Divide " + lhs[num + 1] + " from both sides")
+                            OPERATORS.append("Divide " + lhs[num + 1] + " from both sides")
                             value.append(lhs[num + 1])
                         elif(lhs[num + 2] == "/"):
-                            operators.append("Multiply " + lhs[num + 1] + " from both sides")
+                            OPERATORS.append("Multiply " + lhs[num + 1] + " from both sides")
                             value.append(lhs[num + 1])
                 elif(lhs[num] == "-" and (num + 2 <= array_length - 1) and (lhs[num + 2] == "*" or lhs[num + 2] == "/")):
                     if(num + 2 <= array_length - 1):
                         if(lhs[num + 2] == "*"):
-                            operators.append("Divide both sides by -" + lhs[num + 1])
+                            OPERATORS.append("Divide both sides by -" + lhs[num + 1])
                             value.append(lhs[num + 1])
                         elif(lhs[num + 2] == "/"):
-                            operators.append("Multiply by both sides by -" + lhs[num + 1])
+                            OPERATORS.append("Multiply by both sides by -" + lhs[num + 1])
                             value.append(lhs[num + 1])
                 elif(lhs[num] == "-"):
-                    operators.append("Add " + lhs[num + 1] + " to both sides")
+                    OPERATORS.append("Add " + lhs[num + 1] + " to both sides")
                     value.append(lhs[num + 1])
                 elif(lhs[num] == "+"):
-                    operators.append("Subtract " + lhs[num + 1] + " from both sides")
+                    OPERATORS.append("Subtract " + lhs[num + 1] + " from both sides")
                     value.append(lhs[num + 1])
                 elif(lhs[num] == "*"):
-                    operators.append("Divide " + lhs[num + 1] + " from both sides")
+                    OPERATORS.append("Divide " + lhs[num + 1] + " from both sides")
                     value.append(lhs[num + 1])
                 else:
-                    operators.append("Multiply " + lhs[num + 1] + " from both sides")
+                    OPERATORS.append("Multiply " + lhs[num + 1] + " from both sides")
                     value.append(lhs[num + 1])
 
 def evaluate(index):
     global sum;
-    option = operators[index][:1]
+    option = OPERATORS[index][:1]
+    del OPERATORS[index]
     print(option)
     if(option == "S"):
         option = "+"
@@ -52,6 +76,7 @@ def evaluate(index):
         option = "*"
     array_length = len(lhs)
     for num in range (0, array_length - 2):
+        print(value)
         if(option == '*' or option == '/'):
             if(lhs[num] == value[index] and lhs[num + 1] == option):
                 sign = lhs.pop(num + 1)
@@ -75,11 +100,30 @@ def evaluate(index):
                 sum = sum + number;
         print(lhs)
         print(sum)
+    operatorFunc()
 
+def copy_state(s):
+    return lhs
+
+def goal_test(s):
+  return (lhs[0]=="x" or (lhs[0]=="+" and lhs[1]=="x"))
+
+def get_sum():
+    global sum
+    return sum
+
+def get_operators():
+    global OPERATORS
+    return OPERATORS
+
+
+BRIFL_SVG = True
+def use_BRIFL_SVG():
+  global render_state
+  #from  Missionaries_SVG_VIS_FOR_BRIFL import render_state as rs
+  #render_state = rs
+  from  Missionaries_SVG_VIS_FOR_BRIFL import render_state
 
 operatorFunc()
-print(operators)
+print(OPERATORS)
 print(value)
-evaluate(0)
-evaluate(1)
-evaluate(2)
